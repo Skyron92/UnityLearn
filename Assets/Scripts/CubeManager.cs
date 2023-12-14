9aab4323a8e837e953f5d0566e8ec00d862410f4
Skyron <courtier.leo@gmail.com>
2023-12-14 01:04:37 +0100

    Add CubeSelection

--- Assets/Scripts/CubeManager.cs
+++ Assets/Scripts/CubeManager.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CubeManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private List<CubeUnit> _cubeUnits = new List<CubeUnit>();
+    [SerializeField] private InputActionReference mouseInputActionReference;
+    private InputAction MouseInputAction => mouseInputActionReference.action;
+    [SerializeField, Range(0,100)] private float range;
+    [SerializeField] private LayerMask selectableLayer;
+    
+    void Awake() {
+        MouseInputAction.Enable();
+        MouseInputAction.started += context => SelectCubeUnit();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void SelectCubeUnit() {
+        Ray rayon = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(rayon, out RaycastHit hit, range, selectableLayer)) {
+            CubeUnit unit = hit.transform.gameObject.GetComponent<CubeUnit>();
+            _cubeUnits.Add(unit);
+            unit.OnSelected();
+        }
+        else {
+            foreach (var unit in _cubeUnits) {
+                unit.OnDeselected();
+            }
+            _cubeUnits.Clear();
+        }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(Camera.main.transform.position, range);
     }
 }
---
+++ Assets/Scripts/CubeUnit.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+public class CubeUnit : MonoBehaviour {
+    
+    [SerializeField] private Material baseMaterial, selectedMaterial;
+    private MeshRenderer _meshRenderer;
+
+    private void Awake() {
+        _meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    public void OnSelected() {
+        _meshRenderer.material = selectedMaterial;
+    }
+
+    public void OnDeselected() {
+        _meshRenderer.material = baseMaterial;
+    }
+    
+}
